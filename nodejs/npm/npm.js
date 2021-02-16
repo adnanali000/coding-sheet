e14ac4a623c7c147e4e640cafc7536466dc5d5fd
Adnan musil <adnan.museel123@gmail.com>
2021-02-16 16:49:05 +0500

    npm work

---
+++ nodejs/npm/npm.js
@@ -0,0 +1,12 @@
+//for specific version npm install slugify@1.3.5
+//to install package which is no more used in production --save-dev
+//to use package globally --global
+//to install package npm i package-name 0r npm install package-name
+//to accept patch ~
+//to install latest version in dependecies write > in start of any module
+//to install version uptodate we use ~ in the start of module dependencies
+
+
+console.log('hello nodemon');
+console.log('hello nodemon123');
+console.log('hey updated nodeman');
