b0bdedac829387dfb12d216379547f262ca07fef
Adnan musil <adnan.museel123@gmail.com>
2021-03-02 20:16:41 +0500

    using mongoose in nodejs

--- mongodb/mongoos/index.js
+++ mongodb/mongoos/index.js
@@ -1,10 +1,48 @@
 //mongoose start
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser:true});
+mongoose.connect('mongodb://localhost/adnanMongo',{useNewUrlParser:true});
 
 let db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error: '));
 db.once('open',function(){
     //we're connected
-    console.log('We are connected');
+    //  console.log('We are connected');
+})
+
+//schema
+let kittySchema = new mongoose.Schema({
+    name:String
+});
+
+//method
+kittySchema.methods.speak = function(){
+    var greeting = 'My name is '+this.name;
+    console.log(greeting);
+}
+
+//model
+let kitten = mongoose.model('adnankitty',kittySchema);
+
+//create object
+let adnanKitty = new kitten({name:'adnan kitty'});
+let adnanKitty2 = new kitten({name:'adnan kitty 2'});
+
+// console.log(adnanKitty.name);
+// adnanKitty.speak();
+
+
+//save data
+adnanKitty.save(function(err,adnanKitty){
+    if(err) return console.error(err);
+    // adnanKitty.speak();
+})
+
+adnanKitty2.save(function(err,data){
+    if(err) return console.error(err);
+    // data.speak();
+})
+
+kitten.find({name:'adnan kitty 2'},function(err,kittens){
+    if(err) return console.error(err);
+    console.log(kittens);
 })
