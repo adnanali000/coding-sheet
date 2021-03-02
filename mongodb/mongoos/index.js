f247a8a03d50dcbaf8a68c6b9e4dda0e159672b4
Adnan musil <adnan.museel123@gmail.com>
2021-03-02 19:26:30 +0500

    installed  mongodb compass and installed mongoose

---
+++ mongodb/mongoos/index.js
@@ -0,0 +1,10 @@
+//mongoose start
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test',{useNewUrlParser:true});
+
+let db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error: '));
+db.once('open',function(){
+    //we're connected
+    console.log('We are connected');
+})
